ed913a6576879910e3f149b0cfeca780972f8a73
Franco Gimenez <89666504+frankitook@users.noreply.github.com>
2024-10-25 08:14:55 -0300

    Actualizar MiBotTelegramm.java

--- src/main/java/com/franco/pruebabottel/MiBotTelegramm.java
+++ src/main/java/com/franco/pruebabottel/MiBotTelegramm.java
@@ -51,7 +51,7 @@ public class MiBotTelegramm extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return "6893369527:AAGU1FayZQScj4t3O1VqnMqbeVsPbyr4m4adss8"; 
+        return "your_token"; 
     }
 
 @Override
